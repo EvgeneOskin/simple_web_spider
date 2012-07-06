27c05c56df7917df38feb832b74cde4d5a7fc18b
Evgene Oskin <burnst@mail.ru>
2012-07-06 16:05:15 +0700

    main

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ print('Hello, my master!\n')
 import connector
 
 spider=connector.connector()
-info_msg = 'You seaching {0} in {1} in deep level {2}'.format(
-    spider.data['What'], spider.data['Where'], spider.data['How_deep'])
+info_msg = 'You seaching \"{0}\" in {1} in deep level {2}'.format(
+    spider.data['What'], spider.data['Where'].keys()[0], spider.data['How_deep'])
 print(info_msg)
-spider.connect()
-spider.search()
+spider.deep_search()
 print('I\'m quiting')
